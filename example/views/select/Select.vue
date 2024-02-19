73376d9c837661f5241b1ab72d4e81af0d02ec4a
liulihao <551772063@qq.com>
2024-02-19 14:31:02 +0800

    FEATURE:增加select组件

---
+++ example/views/select/Select.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, getCurrentInstance } from 'vue'
+const { proxy } = getCurrentInstance()
+const selectVal = ref();
+const options = ref([
+  {label: '小月月', value: 'xyy'},
+  {label: '小鑫鑫', value: 'xxx'}
+])
+</script>
+
+<template>
+  <div>
+    <div>example/views/select/Select.vue</div>
+    <o-select
+      v-model="selectVal"
+      :options="options"
+      width="900"
+    ></o-select>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- packages/index.js
+++ packages/index.js
@@ -4,8 +4,9 @@ import Empty from './components/empty/Empty.vue'
 import Title from './components/title/Title.vue'
 import Dialog from './components/dialog/Dialog.vue'
 import Drawer from './components/drawer/Drawer.vue'
+import Select from './components/select/Select.vue'
 
-const comps = [Button, Empty, Title, Dialog, Drawer]
+const comps = [Button, Empty, Title, Dialog, Drawer, Select]
 const finalComps = comps.map((v) => {
   let baseName = v.__name ?? v.name
   let res = 'o' + baseName.charAt(0).toUpperCase() + baseName.slice(1)
