d417fffafda493332d0cf565154ef6853564ce0a
liulihao <551772063@qq.com>
2024-03-14 14:10:21 +0800

    FEATURE:加parseTime

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -195,6 +195,54 @@ export function clone(data) {
 }
 
 /**
+ * 格式化时间为年月日时分秒的格式， 格式可以自定义。
+ * ① 时间戳10位和13位都可以转换成格式化的日期
+ * ② java8格式的日期和有效的日期都可以转换成定义的日期格式
+ * @param {Date, string}  都有默认参数
+ * @example
+ * parseTime() // 2020-07-17 09:53:07
+ * parseTime('2018-02-13T06:17') // 2018-02-13 06:17:00
+ * parseTime('2020/03/02 06:02') // 2020-03-02 06:02:00
+ * parseTime(1541927611000); //2018-11-11 17:13:21
+ * parseTime(1541927611000, "{y}年{m}月{d}日 {h}时{m}分{s}秒"); // 2018年11月11日 17时11分31秒
+ * parseTime(1541927611, "{y}/{m}/{d} {h}:{m}:{s}"); // 2018/11/11 17:11:31
+ * parseTime(new Date()); //2018-11-11 17:13:21
+ * parseTime(new Date().getTime()); //2018-11-11 17:13:21
+ */
+export function parseTime(
+  time = new Date(),
+  cFormat = '{y}-{m}-{d} {h}:{i}:{s}',
+) {
+  let date
+  if (typeof time === 'object') {
+    date = time
+  } else {
+    if (('' + time).length === 10) time = parseInt(time) * 1000
+    date = new Date(time)
+  }
+  const formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay(),
+  }
+  const time_str = cFormat.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+    let value = formatObj[key] // Note: getDay() returns 0 on Sunday
+    if (key === 'a') {
+      return ['日', '一', '二', '三', '四', '五', '六'][value]
+    }
+    if (result.length > 0 && value < 10) {
+      value = '0' + value
+    }
+    return value || 0
+  })
+  return time_str
+}
+
+/**
  * 生成随机数, 第一个参数可传字符串, 空 或者数组
  * uuid("名字") => 名字hc8f
  * uuid() => abcd
