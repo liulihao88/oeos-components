d5d05df312de6a39e49d65d21fd9cf36b786f803
liulihao <551772063@qq.com>
2024-03-27 18:55:12 +0800

    FEATURE:加复制路径的功能

--- docs/.vitepress/theme/logo.vue
+++ docs/.vitepress/theme/logo.vue
@@ -9,6 +9,15 @@
       {{ sourceVisible === true ? '代码折叠' : '代码显示' }}
     </div>
   </el-button>
+
+  <el-button
+    type="primary"
+    size="small"
+    @click.stop.prevent="copyUrl"
+    class="code-toogle dev-copy"
+  >
+    <div class="visible-text">复制路径(仅本地)</div>
+  </el-button>
 </template>
 
 <script lang="ts" setup>
@@ -21,6 +30,21 @@ const toggleSourceVisible = () => {
   setStorage('codeVisible', !sourceVisible.value)
   location.reload()
 }
+const copyUrl = () => {
+  let pathname = location.pathname
+  console.log(`pathname`, pathname)
+  if (!pathname || pathname === '/') {
+    return
+  }
+  let vueStr = pathname.replace('/components/', '') + '.vue'
+  console.log(`vueStr`, vueStr)
+
+  if (navigator.clipboard && window.isSecureContext) {
+    return navigator.clipboard.writeText(vueStr)
+  } else {
+    console.log('浏览器不支持')
+  }
+}
 </script>
 
 <style lang="scss">
@@ -29,5 +53,8 @@ const toggleSourceVisible = () => {
   top: 20px;
   right: 50%;
   z-index: 200;
+}
+.dev-copy {
+  right: calc(50% - 150px);
 }
 </style>
