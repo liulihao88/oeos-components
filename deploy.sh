16106160978d002889f863ec6ca15340708e6396
liulihao <551772063@qq.com>
2024-06-27 17:20:50 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,19 @@ pnpm docsbuild
 # 进入待发布的 dist/ 目录
 cd docs/.vitepress/dist
 
+# 检查是否提供了提交消息
+if [ -z "$1" ]; then
+    # 如果没有提供提交消息，则使用默认消息
+    commit_message="update"
+else
+    # 使用提供的提交消息
+    commit_message="$1"
+fi
+
 # 提交打包静态网站到 github-pages 分支
 git init
 git add .
-git commit -m 'deploy'
+git commit -m "$commit_message"
 
 # 部署到 https://<username>.github.io/<repo>
 git push -f https://github.com/liulihao88/oeos-components.git main:github-pages
