f0f26e7f2bc3fcaaee45a7a8229f4c9db47bdb45
liulihao <551772063@qq.com>
2024-10-29 10:39:38 +0800

    feat: 更新form的属性

--- docs/examples/OForm/usually.vue
+++ docs/examples/OForm/usually.vue
@@ -45,6 +45,7 @@ const fieldList = [
     comp: 'el-input',
     imgAttrs: {
       src: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png',
+      class: ['mr']
     },
     attrs: {
       style: {
