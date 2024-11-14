693cff8c57c089e2551b365159ddcef2394c0fde
liulihao <551772063@qq.com>
2024-11-14 10:14:52 +0800

    feat: 日期

--- docs/examples/ODateRange/usually.vue
+++ docs/examples/ODateRange/usually.vue
@@ -3,17 +3,20 @@ import { ref, getCurrentInstance, h } from 'vue'
 const { proxy } = getCurrentInstance()
 const dateValue = ref()
 
-const customPrefix = {
-  render() {
-    return h('p')
-  },
-}
 </script>
 
 <template>
   <div>
-    <o-date-range v-model="dateValue" width="600"></o-date-range>
-    <o-date-range v-model="dateValue" title="选择日期" start-placeholder="开始" end-placeholder="结束" range-separator="///"></o-date-range>
+    <o-title title="年月日 时分秒"></o-title>
+    <o-date-range v-model="dateValue" width="600" format="YYYY-MM-DD HH:mm:ss" type="datetimerange"></o-date-range>
+    <o-title title="多属性"></o-title>
+    <o-date-range
+      v-model="dateValue"
+      title="选择日期"
+      start-placeholder="开始"
+      end-placeholder="结束"
+      range-separator="///"
+    ></o-date-range>
   </div>
 </template>
 
