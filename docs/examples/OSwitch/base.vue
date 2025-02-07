43900821ba5bec517e634ec2cf0c5311ccc1af68
liulihao <551772063@qq.com>
2025-02-07 12:10:52 +0800

    feat: switch用法

--- docs/examples/OSwitch/base.vue
+++ docs/examples/OSwitch/base.vue
@@ -2,6 +2,35 @@
 import { ref, getCurrentInstance } from 'vue'
 const { proxy } = getCurrentInstance()
 const cc = ref(false)
+const dd = ref(false)
+const loading = ref(false)
+
+async function random() {
+  let random = Math.random()
+  console.log(`37 random`, random)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (random < 0.5) {
+        return resolve()
+      } else {
+        return reject()
+      }
+    }, 2000)
+  })
+}
+
+const beforeChange = async () => {
+  loading.value = true
+  try {
+    await random()
+    return true
+  } catch (e) {
+    console.log(`43 e`, e)
+    return false
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
@@ -9,8 +38,12 @@ const cc = ref(false)
     <o-switch v-model="cc"></o-switch>
     <br />
 
-    <o-switch v-model="cc" active-text="这是active的value"></o-switch>
+    <o-switch v-model="cc" active-text="这是active的text"></o-switch>
     <br />
     <o-switch v-model="cc" active-text="这是active的text" inactive-text="这是inactive的text"></o-switch>
+    <br />
+
+    <o-switch v-model="dd" :before-change="beforeChange" :loading="loading" active-text="异步"></o-switch>
+    
   </div>
 </template>
