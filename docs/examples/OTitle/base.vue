3e0e003c0df02e46bf315b528b63b56a325dbb56
liulihao <551772063@qq.com>
2025-03-04 12:44:59 +0800

    feat: title的用法

--- docs/examples/OTitle/base.vue
+++ docs/examples/OTitle/base.vue
@@ -7,12 +7,14 @@ const { proxy } = getCurrentInstance()
   <div>
     <o-title title="你好"></o-title>
     <o-title title="无左侧树杠" type="simple"></o-title>
-    <o-title title="你好2" inner subTitle="我不服啊">
-      我是左侧
+    <o-title title="你好2" inner subTitle="我不服啊" type="simple">
+      <template #icon>
+        <o-icon name="delete" class="mr"></o-icon>
+      </template>
+      我是左侧slot
       <template #right>
-        <div class="w-600">我是右侧的slot</div>
+        <div class="w-600 tr">我是右侧的slot</div>
       </template>
     </o-title>
-    不行啊弟弟
   </div>
 </template>
