798194c761450b5bc0ce1f7961980c1077429f0a
刘力豪 <liulihao@oct-ea.com>
2025-04-02 15:37:25 +0800

    feat: 图片相关

--- packages/components/empty/src/index.vue
+++ packages/components/empty/src/index.vue
@@ -26,8 +26,8 @@ const props = defineProps({
   },
 })
 /** @使用方式
-<o-empty description="您没有消费订单" size="48"></o-empty>
-<o-empty class="w-100%" src="https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg" size="200" ></o-empty>
+<o-empty description="您没有消费订单" width="48"></o-empty>
+<o-empty class="w-100%" src="https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg" width="200" ></o-empty>
 */
 </script>
 
