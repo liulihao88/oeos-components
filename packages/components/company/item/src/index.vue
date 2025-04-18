7e39326dc7071b7eb94842f521b0b7160835bef3
刘力豪 <liulihao@oct-ea.com>
2025-04-18 15:57:07 +0800

    feat: item的字体变为16px

--- packages/components/company/item/src/index.vue
+++ packages/components/company/item/src/index.vue
@@ -24,25 +24,25 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="item-box" :style="{ ...processWidth(props.width) }">
-    <div class="item-box__img">
+  <div class="o-item-box" :style="{ ...processWidth(props.width) }">
+    <div class="o-item-box__img">
       <slot name="img">
         <img :src="props.src" alt="" />
       </slot>
     </div>
-    <div class="item-box__right">
+    <div class="o-item-box__right">
       <slot name="label">
-        <div class="item_box__label">{{ props.label }}</div>
+        <div class="o-item_box__label">{{ props.label }}</div>
       </slot>
       <slot name="value">
-        <div class="item_box__value">{{ props.value }}</div>
+        <div class="o-item_box__value">{{ props.value }}</div>
       </slot>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.item-box {
+.o-item-box {
   background-color: #fff;
   padding: 16px;
   border-radius: 4px;
@@ -50,26 +50,26 @@ const props = defineProps({
   display: flex;
   height: 100px;
   align-items: center;
+  font-size: 16px;
   justify-content: v-bind('props.src ? "space-between" : "center"');
-  .item-box__img {
+  .o-item-box__img {
     height: 100%;
     margin-right: 8px;
     img {
       height: 100%;
     }
   }
-  .item-box__right {
+  .o-item-box__right {
     display: flex;
     flex-direction: column;
     align-items: center;
     height: 100%;
     justify-content: space-between;
-    .item_box__label {
+    .o-item_box__label {
       color: var(--45);
     }
-    .item_box__value {
+    .o-item_box__value {
       font-weight: 700;
-      font-size: 16px;
     }
   }
 }
