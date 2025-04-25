9fa2ccd2a96e86b0ca46521fead63a5895c3dc34
刘力豪 <liulihao@oct-ea.com>
2025-04-25 17:26:58 +0800

    feat: 更新选择

--- docs/examples/OSelect/disabled.vue
+++ docs/examples/OSelect/disabled.vue
@@ -7,8 +7,10 @@ const options = ref([
   { label: '王五', value: 'w5' },
   { label: '赵六', value: 'z6' },
 ])
-const selectValue = ref([])
+const selectValue1 = ref([])
 const selectValue2 = ref([])
+const selectValue3 = ref([])
+const selectValue4 = ref([])
 const optionsDisabled = (item, index) => {
   return index % 2 === 0
 }
@@ -21,10 +23,18 @@ const options2 = ref(['男人', '女人', '爱情'])
 
 <template>
   <div>
-    <o-select v-model="selectValue" :options="options" multiple :optionsDisabled="optionsDisabled" title="多选正常" />
-
+    <o-select v-model="selectValue1" :options="options" :optionsDisabled="optionsDisabled" title="单选正常" />
     <o-select
       v-model="selectValue2"
+      :options="options2"
+      :optionsDisabled="optionsDisabled2"
+      type="simple"
+      title="单选简单"
+    />
+    <o-select v-model="selectValue3" :options="options" multiple :optionsDisabled="optionsDisabled" title="多选正常" />
+
+    <o-select
+      v-model="selectValue4"
       :options="options2"
       multiple
       type="simple"
