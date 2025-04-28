17eac73cbcf3e0638b429de3649c3fbcf39ae56e
刘力豪 <liulihao@oct-ea.com>
2025-04-28 16:42:41 +0800

    feat: 更新自定义select

--- docs/examples/OSelect/slot.vue
+++ docs/examples/OSelect/slot.vue
@@ -7,15 +7,27 @@ const options = ref([
   { label: '李四', value: 'l4' },
   { label: '王五', value: 'w5' },
 ])
+
+const confirm = (op, item) => {
+  console.log(`74 item`, item)
+  console.log(`38 op`, op)
+}
 </script>
 
 <template>
   <div>
     <o-select v-model="selectValue" :options="options">
       <template #header>自定义头部</template>
+      <template #default="{ options, item }">
+        <div class="f-bt-ct w-100%">
+          <span>{{ item.label }}</span>
+          <o-popconfirm @confirm="confirm(options, item)" trigger="click">
+            <o-icon name="delete" @click.stop></o-icon>
+          </o-popconfirm>
+        </div>
+      </template>
       <template #footer>自定义底部</template>
       <!-- <template #label="{ label, value }">自定义标签内容 {{ label }} ?? {{ value }}</template> -->
-      <template #tag>??????自定义标签内容===</template>
     </o-select>
   </div>
 </template>
