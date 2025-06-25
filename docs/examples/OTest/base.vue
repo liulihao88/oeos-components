88a96d56a3dc88d7744c81f4bb1ab6e30d38b8f8
刘力豪 <liulihao@oct-ea.com>
2025-06-25 14:33:09 +0800

    feat: a

--- docs/examples/OTest/base.vue
+++ docs/examples/OTest/base.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import { ref, getCurrentInstance } from 'vue'
 const { proxy } = getCurrentInstance()
-const cc = ref()
 </script>
 
 <template>
   <div>
     <div>docs/examples/OTest/base.vue</div>
-    <o-input v-model="cc" v-number :max="10333" />
   </div>
 </template>
