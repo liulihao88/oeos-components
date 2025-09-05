db16d63e0dbe77d7e2c8abec3499993d29a3d4d3
liulihao88 <551772063@qq.com>
2025-09-05 16:03:34 +0800

    fix: 更改tooltip的样例

--- docs/examples/OTooltip/html.vue
+++ docs/examples/OTooltip/html.vue
@@ -9,7 +9,7 @@ const { proxy } = getCurrentInstance()
       width="200px"
       raw-content
       content=" <span class='cl-blue m-100'>我是啊</span>
-        我爱你啊1111"
+        我爱你啊1111 <br>这里使用 &lt; br &gt;进行换行 <div>或者直接使用块级元素进行换行</div>"
     >
       这里显示内容
     </o-tooltip>
