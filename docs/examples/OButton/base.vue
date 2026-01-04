c12698e5180cde6c83c37b0c25584b610b507442
liulihao <551772063@qq.com>
2026-01-04 14:03:16 +0800

    fix: 更新button按钮

--- docs/examples/OButton/base.vue
+++ docs/examples/OButton/base.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { ref, getCurrentInstance } from 'vue'
+import { $toast } from '@oeos-components/utils'
 </script>
 
 <template>
   <div>
     <o-button>我是按钮</o-button>
+
+    <o-button size="small" type="primary" :loading="false" :time="1000" @click="$toast('一秒显示一次')">
+      小按钮
+    </o-button>
   </div>
 </template>
--- packages/components/button/src/index.vue
+++ packages/components/button/src/index.vue
@@ -1,10 +1,10 @@
 <template>
   <o-tooltip v-if="content" :content="content" v-bind="tooltipAttrs">
-    <el-button v-bind="$attrs" class="t-button-tip" @click="handleClick">
+    <el-button v-bind="{ loading: loading, ...$attrs }" class="t-button-tip" @click="handleClick" :loading="loading">
       <slot />
     </el-button>
   </o-tooltip>
-  <el-button v-else v-bind="$attrs" @click="handleClick">
+  <el-button v-else v-bind="{ loading: loading, ...$attrs }" @click="handleClick">
     <slot />
   </el-button>
 </template>
@@ -33,6 +33,8 @@ const emits = defineEmits(['click'])
 
 const lastClickTime = ref<number | null>(null)
 
+const loading = ref(false)
+
 const handleClick = () => {
   if (props.time === 0) {
     emits('click')
@@ -40,15 +42,14 @@ const handleClick = () => {
   }
 
   const now = Date.now()
-  if (lastClickTime.value === null) {
-    // 第一次点击
+  if (lastClickTime.value === null || now - lastClickTime.value >= props.time) {
+    loading.value = true
     emits('click')
     lastClickTime.value = now
-  } else {
-    if (now - lastClickTime.value >= props.time) {
-      emits('click')
-      lastClickTime.value = now
-    }
+
+    setTimeout(() => {
+      loading.value = false
+    }, props.time)
   }
 }
 </script>
