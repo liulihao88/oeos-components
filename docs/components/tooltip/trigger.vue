ca540cf071a1dedeb4f62da0719b26bbebed6cb0
liulihao <551772063@qq.com>
2026-01-30 11:52:52 +0800

    fix: 更新tooltip

--- docs/components/tooltip/trigger.vue
+++ docs/components/tooltip/trigger.vue
@@ -9,7 +9,13 @@ const isTest14 = () => {
 <template>
   <div style="width: 1000px">
     <o-tooltip content="我只有在点击的时候才显示文本" trigger="click">
-      <el-button type="primary" @click="isTest14">测试36</el-button>
+      <el-button type="primary" @click="isTest14">click</el-button>
+    </o-tooltip>
+    <o-tooltip content="我只有在点击的时候才显示文本" trigger="contextmenu">
+      <el-button type="primary" @click="isTest14">contextmenu</el-button>
+    </o-tooltip>
+    <o-tooltip content="我只有在点击的时候才显示文本" :trigger="['contextmenu', 'click']">
+      <el-button type="primary" @click="isTest14">['contextmenu', 'click']</el-button>
     </o-tooltip>
   </div>
 </template>
