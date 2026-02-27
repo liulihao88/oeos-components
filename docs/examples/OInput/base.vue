844419c7b5c075db7afc9eb4b2619fb5b4d7c93b
liulihao <551772063@qq.com>
2026-02-27 10:29:56 +0800

    fix: 去掉examples文件夹

--- docs/examples/OInput/base.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <o-input v-model="input" style="width: 240px" placeholder="Please input" />
-</template>
-
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const input = ref('')
-</script>
--- docs/examples/OInput/clearable.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <el-input v-model="input" style="width: 240px" placeholder="Please input" :clearable="false" />
-</template>
-
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const input = ref('')
-</script>
