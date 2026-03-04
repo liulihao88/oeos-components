e968d5b7df8d2664cf4659c0e3850cb8bf551f9b
liulihao <551772063@qq.com>
2026-03-04 10:53:15 +0800

    fix: 更新icon

--- docs/components/icon/base.vue
+++ docs/components/icon/base.vue
@@ -7,32 +7,47 @@ function tt() {
 function isTest13() {
   console.log('isTest13')
 }
+
+const iconLists = ref([
+  'edit',
+  'check',
+  'delete',
+  'warning',
+  'refresh',
+  'close',
+  'add-location',
+  'arrow-up',
+  'arrow-right',
+  'arrow-down',
+  'arrow-left',
+  'plus',
+  'circle-close',
+  'more',
+  'zoom-in',
+  'zoom-out',
+  'lock',
+  'unlock',
+])
 </script>
 
 <template>
   <div>
-    <o-icon name="delete" class="po-a l-200 m-r-20" cc="1" size="20" @click="tt" content="删除"></o-icon>
+    <o-icon name="delete"></o-icon>
     <o-icon name="warning" content="警告"></o-icon>
-    <o-icon name="refresh" content="刷新" @click="tt" color="var(--blue)" size="50" placement="right"></o-icon>
-    <o-icon name="refresh" content="刷新" class="cl-blue"></o-icon>
-    <o-icon name="edit" content="编辑" size="30"></o-icon>
-    <o-icon name="check"></o-icon>
+    <o-icon name="refresh" content="刷新" @click="tt" color="var(--yellow)" size="50" placement="right"></o-icon>
     <o-icon name="close" class="is-loading"></o-icon>
-    <o-icon name="download"></o-icon>
-    <o-icon name="arrow-up"></o-icon>
-    <o-icon name="arrow-right"></o-icon>
-    <o-icon name="arrow-down"></o-icon>
-    <o-icon name="arrow-left"></o-icon>
-    <o-icon name="plus"></o-icon>
-    <o-icon name="circle-close"></o-icon>
-    <o-icon name="close"></o-icon>
-    <o-icon name="more"></o-icon>
-    <o-icon name="zoom-in"></o-icon>
-    <o-icon name="zoom-out"></o-icon>
-    <o-icon name="lock" content="锁定"></o-icon>
-    <o-icon name="unlock" content="未锁定"></o-icon>
+    <o-icon name="refresh" content="刷新" class="cl-blue"></o-icon>
 
-    <div>支持首字母大写, 驼峰, 中划线命名</div>
+    <hr>
+
+    <div>
+      <template v-for="(v, i) in iconLists" :key="i">
+        <o-icon :name="v" :content="v"></o-icon>
+      </template>
+    </div>
+
+    <hr>
+    <o-title title="支持首字母大写, 驼峰, 中划线命名"></o-title>
     <o-icon name="AddLocation"></o-icon>
     <o-icon name="addLocation"></o-icon>
     <o-icon name="add-location"></o-icon>
