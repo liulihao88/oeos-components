923953f5f6622b50da9c7c92f96109eaa7ec273d
liulihao <551772063@qq.com>
2026-03-11 11:35:47 +0800

    fix: 单选

--- docs/components/table/click.vue
+++ docs/components/table/click.vue
@@ -7,10 +7,26 @@ const data: any = ref([])
 const selectRow = ref({})
 const tableRef = ref(null)
 
+const pageNumber = ref(1)
+const pageSize = ref(10)
+
+const indexMethod = (index) => {
+  // 如果当前页是最后一页（数据量不足 pageSize），则基于实际数据量计算
+  return (pageNumber.value - 1) * pageSize.value + index + 1
+}
+
 const columns = [
   {
-    type: 'radio', // 添加radio类型列
+    prop: 'radio', // 添加radio类型列
     width: 50,
+    align: 'center',
+    useSlot: true,
+  },
+  {
+    type: 'index',
+    width: 70,
+    label: '序号',
+    index: indexMethod,
   },
   {
     label: '人物单',
@@ -63,7 +79,11 @@ async function editRow(row, scope, evt) {
 
 async function init() {
   await proxy.sleep(0)
-  data.value = [{ name: 'andy1' }, { name: 'andy2' }, { name: 'andy3' }]
+  data.value = [
+    { id: 1, name: 'andy1' },
+    { id: 2, name: 'andy2' },
+    { id: 3, name: 'andy3' },
+  ]
   _handleRowClick()
 }
 init()
@@ -102,18 +122,22 @@ const _handleRowClick = () => {
 
 <template>
   <div>
+    {{ selectRow }}
     <o-table
       ref="tableRef"
       :columns="columns"
       :data="data"
+      :showIndex="false"
       :showPage="false"
       height="100%"
       highlight-current-row
       @current-change="handleCurrentChange"
     >
-  <template #plus>
-    <el-button icon="plus" type="text">添加</el-button>
-  </template>
-  </o-table>
+      <template #radio="{ value, row }">
+        <div class="f-ct-ct w-100%">
+          <el-radio v-model="selectRow.id" :value="row.id"></el-radio>
+        </div>
+      </template>
+    </o-table>
   </div>
 </template>
